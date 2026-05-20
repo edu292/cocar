14ce9cdd9693aa1003b61202f4555857434d1fa6
andrempg <ampg.andre@gmail.com>
2026-05-20 10:03:51 -0300

    fix seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use App\Models\Organizacao;
-use App\Models\PerfilMotorista;
-use App\Enums\TipoUsuario;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,44 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $organizacao = Organizacao::create([
-            'nome' => 'Empresa Teste',
-            'cnpj' => '12345678900',
-            'dominio_email' => 'teste.com',
-        ]);
-
-        $senhaPadrao = Hash::make('SenhaForte!');
-
-        User::create([
-            'name' => 'Admin Teste',
-            'email' => 'admin@teste.com',
-            'password' => $senhaPadrao,
-            'cpf' => '12312312300',
-            'tipo' => TipoUsuario::AdministradorOrganizacao,
-            'organizacao_id' => $organizacao->id,
-        ]);
-
-        User::create([
-            'name' => 'Passageiro Teste',
-            'email' => 'passageiro@teste.com',
-            'password' => $senhaPadrao,
-            'cpf' => '12312312301',
-            'tipo' => TipoUsuario::Padrao,
-            'organizacao_id' => $organizacao->id,
-        ]);
-
-        $motoristaUser = User::create([
-            'name' => 'Motorista Teste',
-            'email' => 'motorista@teste.com',
-            'password' => $senhaPadrao,
-            'cpf' => '12312312302',
-            'tipo' => TipoUsuario::Padrao,
-            'organizacao_id' => $organizacao->id,
-        ]);
-
-        PerfilMotorista::query()->create([
-            'user_id' => $motoristaUser->id,
-            'cnh' => '12345678911'
-        ]);
+        $this->call(fluxoTesterSeeder::class);
+        $this->call(AdminSistemaSeeder::class);
     }
 }
